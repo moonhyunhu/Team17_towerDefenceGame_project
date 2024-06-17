672e1a9c7a4d3632a931fbf1c076a64a30580774
moonhyunhu <ansgusgn@gmaile.com>
2024-06-17 14:31:14 +0900

    초기세팅

--- src/app.js
+++ src/app.js
@@ -8,6 +8,7 @@ const PORT = 5555;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(express.static('tower_defence_client'));
 
 server.listen(PORT, async () => {
   console.log(`Server is running on port ${PORT}`);
